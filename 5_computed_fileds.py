8dc1b60b7c8b23311a47f240a2b4156acedef581
Kazi Md. Rakibul Hassan <kazrkb@gmail.com>
2026-04-09 23:18:19 +0600

    computed fileds calculated bmi

--- 5_computed_fileds.py
+++ 5_computed_fileds.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator, model_validator
+from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator, model_validator, computed_field
 from typing import List, Dict, Optional, Annotated
 
 class Patient(BaseModel):
@@ -6,29 +6,37 @@ class Patient(BaseModel):
     email: EmailStr
     age: int
     weight: float
+    height: float
     married: bool
-    allergies: Annotated[Optional[List[str]], Field(default=None, max_length=5)]
+    allergies: List[str]
     contact_address: Dict[str,str]
 
-    @model_validator(mode='after')
-    def validate_emergency_contact(cls,model):
-        if model.age>60 and 'emergency' not in model.contact_address:
-            raise ValueError('Emergency contact is required for patients above 60 years old')
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi = round(self.weight/(self.height**2),2)
+        return bmi
+
+
 
 
 def update_patient_data(patient: Patient):
     print(patient.name)
     print(patient.age)
     print(patient.contact_address)
+    print(patient.bmi)
 
 patient_info = {
     'name': 'Alex',
     'email': '123@gmail.com',
     'age': '90',
     'weight': 78.9,
+    'height': 1.5,
     'married': False,
     'allergies': ['dust', 'pollen'],
     'contact_address': {'emergency': '01845674568'}
 }
 patient1 = Patient(**patient_info)
 update_patient_data(patient1)
+
+
